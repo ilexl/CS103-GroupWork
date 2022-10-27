4bd8dfbbb1911b2163798613e3d9402808de7ded
Alex <109491531+ilexl@users.noreply.github.com>
2022-10-27 13:13:41 +1300

    no change

--- SchoolSystemApplication/SchoolSystemApplication/SchoolSystemApplication.cpp
+++ SchoolSystemApplication/SchoolSystemApplication/SchoolSystemApplication.cpp
@@ -1,21 +1,9 @@
-// SchoolSystemApplication.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
+// header files
 #include <iostream>
 
+// main function
 int main()
 {
-    std::cout << "im the better Alex no I't the better Alecx welcome to that studio\n";
-    std::cout << "new line example\n";
+
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
